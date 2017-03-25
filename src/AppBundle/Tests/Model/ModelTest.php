c891b42a5b7fa4d2a5a665f6cc9430c768949928
Colin Luzio <colinluzio@yahoo.co.uk>
2017-03-25 17:01:59 +0000

    update test

--- src/AppBundle/Tests/Model/ModelTest.php
+++ src/AppBundle/Tests/Model/ModelTest.php
@@ -1,31 +1,31 @@
 <?php
-//use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-//
-//class MyModelTest extends WebTestCase
-//{
-//    /**
-//     * @var EntityManager
-//     */
-//    private $_em;
-//
-//    protected function setUp()
-//    {
-//        $kernel = static::createKernel();
-//        $kernel->boot();
-//        $this->_em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-//        $this->_em->beginTransaction();
-//    }
-//
-//    /**
-//     * Rollback changes.
-//     */
-//    public function tearDown()
-//    {
-//        $this->_em->rollback();
-//    }
-//
-//    public function testRepository()
-//    {
-//        $url = $this->_em->getRepository('AppBundle:Url')->find(1);
-//    }
-//}
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MyModelTest extends WebTestCase
+{
+    /**
+     * @var EntityManager
+     */
+    private $_em;
+
+    protected function setUp()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $this->_em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $this->_em->beginTransaction();
+    }
+
+    /**
+     * Rollback changes.
+     */
+    public function tearDown()
+    {
+        $this->_em->rollback();
+    }
+
+    public function testRepository()
+    {
+        $url = $this->_em->getRepository('AppBundle:Url')->find(1);
+    }
+}
